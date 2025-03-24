4a1c406330d28c081c0274645c5405cbd6e48041
SkipaKS <kseniamasterova2003@gmail.com>
2025-03-24 14:09:54 +0300

    Create void main

---
+++ Skipa_LR3_project.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void FirstSymbol()
+{
+    // ввести первый символ ch1 
+}
+
+void SecondSymbol()
+{
+    // ввести второй символ ch2 
+}
+
+void printASCIICodes()
+{
+
+}
+
+void ptintSumASCIICodes()
+{
+
+}
